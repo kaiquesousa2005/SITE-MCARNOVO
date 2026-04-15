7e1fa3c6b2e93bbc14a2f6aa4f9b94bbf1185628
kaiquesousa2005 <kaique.freire@hotmail.com>
2026-04-15 12:57:34 -0300

    add google ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,18 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17819096493" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17819096493');
+            `,
+          }}
+        />
         {/* Meta Pixel Code */}
         <script
           dangerouslySetInnerHTML={{
